dc55e93298d33a31d34ef99f2cf888dd77c66bfa
Samantha <samantha.goldstein31@gmail.com>
2015-07-22 17:18:39 -0400

    Small modifications to make more modular

--- generate_quote.js
+++ generate_quote.js
@@ -4,22 +4,28 @@ var twitterAPI = require('node-twitter-api');
 var util = require('util');
 
 var MarkovChain = require('markovchain').MarkovChain
-  , quotes1 = new MarkovChain({ files: [ 'data/lebron_sample.txt', 'data/lebron_sample2.txt']  })
-  , quotes2 = new MarkovChain({ files: [ 'data/ulysses_sample.txt', 'data/ulysses_sample2.txt']  })
+  // , quotes1 = new MarkovChain({ files: [ 'data/lebron_sample.txt', 'data/lebron_sample2.txt']  })
+  // , quotes2 = new MarkovChain({ files: [ 'data/ulysses_sample.txt', 'data/ulysses_sample2.txt']  })
   // , quotes = new MarkovChain({ files: [ 'brit_data/bspears_sample.txt', 'brit_data/shakes_sample.txt']  })
 
+  , quotes1 = new MarkovChain({ files:  'brit_data/bspears_sample.txt' })
+  , quotes2 = new MarkovChain({ files:  'brit_data/shakes_sample.txt'})
+  , singleCorpus = new MarkovChain({ files:  ['brit_data/bspears_sample.txt','brit_data/shakes_sample.txt']})
 
-function getTweet() {
+function getTweetTwoCorpus(corpus1, corpus2) {
 
 
   var ul_str =  "His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir Jesus wept: and no wonder, by Christ!";
   var ul_ls = ul_str.split(' ');
-  quotes1
-    .start(function(){
+  corpus1
+  .start(function(){
       //THIS SHOULD BE ITS OWN FUNCTION
       // var lb_str =  "His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir Jesus wept: and no wonder, by Christ!";
       var lb_str = "I think I decided this morning. I mean, I decided this morning I went day to day. I wake up one morning, it's this team. I wake up another morning, it's this team. And it's a process that I felt it was I may feel like this is the best opportunity for me or not the best opportunity for me.";
 
+
+
+      var lb_str = "Baby, can't you see I'm calling A guy like you should wear a warning It's dangerous I'm falling There's no escape I can't wait I need a hit Baby, give me it You're dangerous I'm loving it Too high Can't come down Losin' my head Spinnin' 'round and 'round Do you feel me now? Oh, The taste of your lips I'm on a ride You're toxic I'm slippin' under With a taste of a poison paradise";
       var ls = lb_str.split(" ") //.concat(lb_str.split(" "));
       //console.log(ls);
       // var lb_ls = lb_str.split(" ");
@@ -31,7 +37,7 @@ function getTweet() {
       var s_ls = s.split(" ");
       var last_word = s_ls.slice(s_ls.length-1, s_ls.length);
       var prom = new Promise(function(resolve, reject) {
-        quotes2
+        corpus2
           .start(getWord(last_word,ul_ls)) //
           .end(7)
           .process(function(err, s) {
@@ -44,7 +50,7 @@ function getTweet() {
         var st = s+' '+result;
         // console.log(s+"\tSPACE\t"+result); // "Stuff worked!"
         if (st.length < 140){
-          main(st);
+          testMain(st);
         }
       }, function(err) {
         console.log(err); // Error: "It broke"
@@ -63,11 +69,42 @@ function getWord(word, text) {
   }
 }
 
-getTweet();
+
+function getTweetSingleCorpus(corpus) {
+
+
+  var ul_str =  "His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir Jesus wept: and no wonder, by Christ!";
+  var ul_ls = ul_str.split(' ');
+  corpus
+  .start(function(){
+      //THIS SHOULD BE ITS OWN FUNCTION
+      // var lb_str = "I think I decided this morning. I mean, I decided this morning I went day to day. I wake up one morning, it's this team. I wake up another morning, it's this team. And it's a process that I felt it was I may feel like this is the best opportunity for me or not the best opportunity for me.";
+      var lb_str = "Baby, can't you see I'm calling A guy like you should wear a warning It's dangerous I'm falling There's no escape I can't wait I need a hit Baby, give me it You're dangerous I'm loving it Too high Can't come down Losin' my head Spinnin' 'round and 'round Do you feel me now? Oh, The taste of your lips I'm on a ride You're toxic I'm slippin' under With a taste of a poison paradise";
+      var ls = lb_str.split(" ") //.concat(lb_str.split(" "));
+      //console.log(ls);
+      // var lb_ls = lb_str.split(" ");
+      return ls[Math.floor(Math.random()*ls.length)];
+      // return 'the';
+    })
+    .end(7)
+    .process(function(err, s) {
+      testMain(s);
+
+    });
+}
+
+
+
+getTweetSingleCorpus(singleCorpus);
+
+getTweetTwoCorpus(quotes1, quotes2); //, wl1, wl2);
 
 
 function testMain(st) {
+
+  console.log("\n************************\n");
   console.log(st);
+  console.log("\n************************\n");
 }
 
 
