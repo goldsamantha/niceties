14a5922f350dbc054ac85031207dba2753dfe3b5
Samantha <samantha.goldstein31@gmail.com>
2015-07-28 16:53:22 -0400

    successfully curried

--- gen_with_waterfall.js
+++ gen_with_waterfall.js
@@ -16,34 +16,21 @@ var MarkovChain = require('markovchain').MarkovChain
 function getTweetTwoCorpus(corpus1, corpus2, cb) {
 
 
-  var ul_str =  "His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir Jesus wept: and no wonder, by Christ!";
-  var ul_ls = ul_str.split(' ');
   corpus1
-  .start(function(){
-      //THIS SHOULD BE ITS OWN FUNCTION
-      // var lb_str =  "His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir Jesus wept: and no wonder, by Christ!";
-      var lb_str = "I think I decided this morning. I mean, I decided this morning I went day to day. I wake up one morning, it's this team. I wake up another morning, it's this team. And it's a process that I felt it was I may feel like this is the best opportunity for me or not the best opportunity for me.";
+  .start(getRandomWordFromList)
+  .end(7)
+  .process(function(err, s) {
+    var s_ls = s.split(" ");
+    var last_word = s_ls.slice(s_ls.length-1, s_ls.length);
+    var curry = getUseableWord(last_word)
 
-
-
-      var lb_str = "Baby, can't you see I'm calling A guy like you should wear a warning It's dangerous I'm falling There's no escape I can't wait I need a hit Baby, give me it You're dangerous I'm loving it Too high Can't come down Losin' my head Spinnin' 'round and 'round Do you feel me now? Oh, The taste of your lips I'm on a ride You're toxic I'm slippin' under With a taste of a poison paradise";
-      var ls = lb_str.split(" ") //.concat(lb_str.split(" "));
-      //console.log(ls);
-      // var lb_ls = lb_str.split(" ");
-      return ls[Math.floor(Math.random()*ls.length)];
-      // return 'the';
-    })
-    .end(7)
-    .process(function(err, s) {
-      var s_ls = s.split(" ");
-      var last_word = s_ls.slice(s_ls.length-1, s_ls.length);
-      var prom = new Promise(function(resolve, reject) {
-        corpus2
-          .start(getWord(last_word,ul_ls)) //
-          .end(7)
-          .process(function(err, s) {
-            return resolve(s);
-          });
+    var prom = new Promise(function(resolve, reject) {
+      corpus2
+        .start(curry)
+        .end(7)
+        .process(function(err, s) {
+          return resolve(s);
+        });
 
 
       });
@@ -82,7 +69,25 @@ function getRandomWordFromList(wordList) {
   return wrd;
 }
 
+function getUseableWord(word_to_try, wordList) {
+  // body...
+  // console.log("TRYING: ", word_to_try);
+  return function(wordList){
+    var isInList = (word_to_try in wordList)
+    // console.log("Is in list?: ", isInList);
+    // console.log("LIST: ", wordList);
+    if (isInList){
+      return word_to_try[0];
+    }
+    // else{
+      var tmpList = Object.keys(wordList);
+      var wrd = tmpList[Math.floor(Math.random()*tmpList.length)];
+      return wrd;
+    // }
 
+
+  };
+};
 
 function getWord(word, text) {
   //word is the word to search for and text is the text to look in
@@ -99,7 +104,7 @@ function getWord(word, text) {
 getTweetSingleCorpus(quotes2, testMain);
 // getTweetSingleCorpus(singleCorpus, testMain);
 
-// getTweetTwoCorpus(quotes1, quotes2, testMain); //, wl1, wl2);
+getTweetTwoCorpus(quotes1, quotes2, testMain); //, wl1, wl2);
 
 
 
