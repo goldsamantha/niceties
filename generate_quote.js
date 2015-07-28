75e79f850d328b4719cb393a1f2eef39fd7c9beb
Samantha <samantha.goldstein31@gmail.com>
2015-07-28 17:42:36 -0400

    Modified all for successful markov generated tweets

--- generate_quote.js
+++ generate_quote.js
@@ -2,6 +2,13 @@
 
 var twitterAPI = require('node-twitter-api');
 var util = require('util');
+var async = require('async');
+
+
+
+
+var jjoyce_text = ['data/jjoyce_artist_full.txt', 'data/jjoyce_ulysses_full.txt'];
+var lebron_text = [ 'data/lebron_sample.txt', 'data/lebron_sample2.txt'];
 
 var MarkovChain = require('markovchain').MarkovChain
   // , quotes1 = new MarkovChain({ files: [ 'data/lebron_sample.txt', 'data/lebron_sample2.txt']  })
@@ -9,48 +16,49 @@ var MarkovChain = require('markovchain').MarkovChain
   // , quotes = new MarkovChain({ files: [ 'brit_data/bspears_sample.txt', 'brit_data/shakes_sample.txt']  })
 
   , quotes1 = new MarkovChain({ files:  'brit_data/bspears_sample.txt' })
-  , quotes2 = new MarkovChain({ files:  'brit_data/shakes_sample.txt'})
-  , singleCorpus = new MarkovChain({ files:  ['brit_data/bspears_sample.txt','brit_data/shakes_sample.txt']})
-
-function getTweetTwoCorpus(corpus1, corpus2) {
+  , quotes2 = new MarkovChain({ files:  'brit_data/full_shakespeare.txt'})
+  , singleCorpus = new MarkovChain({ files:  ['brit_data/bspears_sample.txt','brit_data/full_shakespeare.txt']})
 
 
-  var ul_str =  "His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir Jesus wept: and no wonder, by Christ!";
-  var ul_ls = ul_str.split(' ');
+  , joyce = new MarkovChain({ files: jjoyce_text})
+  , lebron = new MarkovChain({ files: lebron_text})
+  , joyce_and_lebron = new MarkovChain({ files: jjoyce_text.concat(lebron_text) });
+
+
+function getTweetTwoCorpus(corpus1, corpus2, cb) {
+
+
   corpus1
-  .start(function(){
-      //THIS SHOULD BE ITS OWN FUNCTION
-      // var lb_str =  "His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir Jesus wept: and no wonder, by Christ!";
-      var lb_str = "I think I decided this morning. I mean, I decided this morning I went day to day. I wake up one morning, it's this team. I wake up another morning, it's this team. And it's a process that I felt it was I may feel like this is the best opportunity for me or not the best opportunity for me.";
+  .start(getRandomWordFromList)
+  .end(7)
+  .process(function(err, s) {
+    var s_ls = s.split(" ");
+    var last_word = s_ls.slice(s_ls.length-1, s_ls.length)[0];
+    var curry = getUseableWord(last_word)
 
-
-
-      var lb_str = "Baby, can't you see I'm calling A guy like you should wear a warning It's dangerous I'm falling There's no escape I can't wait I need a hit Baby, give me it You're dangerous I'm loving it Too high Can't come down Losin' my head Spinnin' 'round and 'round Do you feel me now? Oh, The taste of your lips I'm on a ride You're toxic I'm slippin' under With a taste of a poison paradise";
-      var ls = lb_str.split(" ") //.concat(lb_str.split(" "));
-      //console.log(ls);
-      // var lb_ls = lb_str.split(" ");
-      return ls[Math.floor(Math.random()*ls.length)];
-      // return 'the';
-    })
-    .end(7)
-    .process(function(err, s) {
-      var s_ls = s.split(" ");
-      var last_word = s_ls.slice(s_ls.length-1, s_ls.length);
-      var prom = new Promise(function(resolve, reject) {
-        corpus2
-          .start(getWord(last_word,ul_ls)) //
-          .end(7)
-          .process(function(err, s) {
-            return resolve(s);
-          });
+    var prom = new Promise(function(resolve, reject) {
+      corpus2
+        .start(curry)
+        .end(7)
+        .process(function(err, s) {
+          return resolve(s);
+        });
 
 
       });
       prom.then(function(result) {
-        var st = s+' '+result;
+        loc = result.search(last_word)
+        var st = "";
+        if (loc == 0){
+          // console.log("RESULT: ", result, "Length of last word: ", last_word.length);
+          st = s + " " +result.slice(last_word.length, result.length);
+        }
+        else {
+          st = s+' '+result;
+        }
         // console.log(s+"\tSPACE\t"+result); // "Stuff worked!"
         if (st.length < 140){
-          testMain(st);
+          cb(st);
         }
       }, function(err) {
         console.log(err); // Error: "It broke"
@@ -58,6 +66,47 @@ function getTweetTwoCorpus(corpus1, corpus2) {
 
     });
 }
+
+function getTweetSingleCorpus(corpus, cb) {
+
+
+
+
+  var ul_str =  "His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir Jesus wept: and no wonder, by Christ!";
+  var ul_ls = ul_str.split(' ');
+  corpus
+    .start(getRandomWordFromList)
+    .end(7)
+    .process(function(err, s) {
+      cb(s);
+
+    });
+}
+
+function getRandomWordFromList(wordList) {
+  var tmpList = Object.keys(wordList);
+  var wrd = tmpList[Math.floor(Math.random()*tmpList.length)];
+  return wrd;
+}
+
+function getUseableWord(word_to_try, wordList) {
+  // console.log("TRYING: ", word_to_try);
+  return function(wordList){
+    var isInList = (word_to_try in wordList)
+    // console.log("Is in list?: ", isInList);
+    // console.log("LIST: ", wordList);
+    if (isInList){
+      return word_to_try;
+    }
+    // else{
+      var tmpList = Object.keys(wordList);
+      var wrd = tmpList[Math.floor(Math.random()*tmpList.length)];
+      return wrd;
+    // }
+
+
+  };
+};
 
 function getWord(word, text) {
   //word is the word to search for and text is the text to look in
@@ -70,33 +119,27 @@ function getWord(word, text) {
 }
 
 
-function getTweetSingleCorpus(corpus) {
+
+// getTweetSingleCorpus(quotes2, testMain);
+// getTweetTwoCorpus(quotes1, quotes2, testMain);
+
+// getTweetSingleCorpus(joyce_and_lebron, testMain);
+// getTweetTwoCorpus(lebron, joyce, testMain);
+
+getTweetTwoCorpus(lebron, joyce, pushTweet);
 
 
 
 
-  var ul_str =  "His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir Jesus wept: and no wonder, by Christ!";
-  var ul_ls = ul_str.split(' ');
-  corpus
-  .start(function(){
-      //THIS SHOULD BE ITS OWN FUNCTION
-      // var lb_str = "I think I decided this morning. I mean, I decided this morning I went day to day. I wake up one morning, it's this team. I wake up another morning, it's this team. And it's a process that I felt it was I may feel like this is the best opportunity for me or not the best opportunity for me.";
-      var lb_str = "Baby, can't you see I'm calling A guy like you should wear a warning It's dangerous I'm falling There's no escape I can't wait I need a hit Baby, give me it You're dangerous I'm loving it Too high Can't come down Losin' my head Spinnin' 'round and 'round Do you feel me now? Oh, The taste of your lips I'm on a ride You're toxic I'm slippin' under With a taste of a poison paradise";
-      var ls = lb_str.split(" ") //.concat(lb_str.split(" "));
-      return ls[Math.floor(Math.random()*ls.length)];
-      // return 'the';
-    })
-    .end(7)
-    .process(function(err, s) {
-      // helper(s);
-      testMain(s);
 
-    });
-}
 
-getTweetSingleCorpus(singleCorpus);
-getTweetTwoCorpus(quotes1, quotes2); //, wl1, wl2);
+/*
 
+
+Pushing Function
+
+
+*/
 
 function testMain(st) {
 
@@ -106,7 +149,7 @@ function testMain(st) {
 }
 
 
-function main(s) {
+function pushTweet(s) {
     var accessToken = process.argv[4];
     var tokenSecret = process.argv[5];
 
