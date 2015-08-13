f34221ab8c37075ea382b6f33c3ee9a5efd1caec
Tim Sell <trsell@gmail.com>
2015-08-13 13:11:43 -0400

    whitelist to available datas

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,19 @@ var router = express.Router();
 var generator = require('../generate_quote');
 var _ = require('lodash')
 
+var fs = require('fs');
+
+function getStaff() {
+  return _.map(fs.readdirSync('./data'), function(filename) {
+    return /(.*).txt/.exec(filename)[1];
+  });
+}
 
 /* GET home page. */
 router.get('/:name', function(req, res, next) {
-  if (_.contains(['tom', 'mary', 'dave', 'sonali', 'nick', 'zack'], req.params.name)) {
+  var staff = getStaff()
+  console.log(staff);
+  if (_.contains(staff, req.params.name)) {
     generator.getTweetSingleCorpus('data/' + req.params.name + '.txt', function(salt) {
       // a salt is actually a nice tie
       res.type('application/json');
