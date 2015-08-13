f64fa41b8d05781c4d96fb5b02d8a618efe1eaa2
Tim Sell <trsell@gmail.com>
2015-08-13 12:16:46 -0400

    tom

--- api.js
+++ api.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var http = require('http');
+var generator = require('./generate_quote');
 
 var app = express();
+
+app.get('/tom', function (req, res) {
+  generator.getTweetSingleCorpus(rcFac, testMain)
+});
+
+app.get('/mary', function (req, res) {
+
+});
+
+
 
 app.listen(3000, function () {
   console.log("Thing app is running at localhost:" + 3000);
